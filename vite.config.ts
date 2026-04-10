69e0edc85c70f51863b8c4524bdb71ee0fbbc468
nt-arturleite <arturleite@nutrium.com>
2026-04-10 17:21:44 +0100

    chore: set up vitest for unit testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -8,5 +9,8 @@ export default defineConfig({
     watch: {
       ignored: ['**/db.json'],
     },
+  },
+  test: {
+    globals: true,
   },
 })
